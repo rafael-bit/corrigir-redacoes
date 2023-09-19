2871e10201cc08d762cb1eafc7a70cb9e3c4ae81
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2023-09-19 16:27:02 -0300

    style and localstorage updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,26 @@
 const redacao = document.getElementById("redacao");
 const result = document.getElementById("result");
 const submitButton = document.getElementById("submitButton");
+const clearButton = document.getElementById("clearButton");
 
-redacao.addEventListener("keypress", (e) => {
-	if (redacao.value && e.key === "Enter") SendQuestion();
+clearButton.addEventListener("click", () => {
+	localStorage.removeItem("conversas");
+	result.value = "";
 });
+
+const conversasSalvas = localStorage.getItem("conversas");
+if (conversasSalvas) {
+	result.value = conversasSalvas;
+}
 
 submitButton.addEventListener("click", () => {
 	SendQuestion();
 });
 
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = "";
 
 function SendQuestion() {
-	var sQuestion = redacao.value;
+  let sQuestion = redacao.value;
 
 	fetch("https://api.openai.com/v1/completions", {
 		method: "POST",
@@ -21,40 +28,35 @@ function SendQuestion() {
 			Accept: "application/json",
 			"Content-Type": "application/json",
 			Authorization: "Bearer " + OPENAI_API_KEY,
-		},
+    },
 		body: JSON.stringify({
 			model: "text-davinci-003",
-			prompt: "Corrija a redação com base nos quesitos do enem, pontuando e mostrando o que pode melhorar" + sQuestion,
+			prompt: "Corrija minha redação a seguir com base nos quesitos do enem, pontuando e mostrando o que pode melhorar" + sQuestion,
 			max_tokens: 4000,
 			temperature: 0.5,
-		}),
-	})
-		.then((response) => response.json())
-		.then((json) => {
-			if (result.value) result.value += "\n";
-
+    }),
+  })
+  .then((response) => response.json())
+	.then((json) => {
+    if (result.value) result.value += "\n";
 			if (json.error?.message) {
 				result.value += `Error: ${json.error.message}`;
-			} else if (json.choices?.[0].text) {
-				var text = json.choices[0].text || "Sem resposta";
-
-				result.value += "Chat GPT: " + text;
-			}
-
-			result.scrollTop = result.scrollHeight;
-		})
-		.catch((error) => console.error("Error:", error))
-		.finally(() => {
-			redacao.value = "";
-			redacao.disabled = false;
-			redacao.focus();
-		});
-
-	if (result.value) result.value += "\n\n\n";
-
-	result.value += `Eu: ${sQuestion}`;
-	redacao.value = "Carregando...";
-	redacao.disabled = true;
-
+      } else if (json.choices?.[0].text) {
+          let text = json.choices[0].text || "Sem resposta";
+						result.value += "Correção: " + text;
+        }
+						result.scrollTop = result.scrollHeight;
+						localStorage.setItem("conversas", result.value);
+  })
+  .catch((error) => console.error("Error:", error))
+  .finally(() => {
+		redacao.value = "";
+		redacao.disabled = false;
+		redacao.focus();
+  });
+		if (result.value) result.value += "\n\n\n";
+			result.value += `Redação: ${sQuestion}`;
+			redacao.value = "Carregando...";
+			redacao.disabled = true;
 	result.scrollTop = result.scrollHeight;
 }
